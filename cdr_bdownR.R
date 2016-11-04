b3b5184d216a706829e4ec5b457ebb044dc7e110
Bartesto <barthuntley@iinet.net.au>
2016-11-04 08:11:41 +0800

    added loop for targeting specific orders

--- cdr_bdownR.R
+++ cdr_bdownR.R
@@ -1,12 +1,12 @@
 
 #Python script and path
-pyPath <- "Z:\\DOCUMENTATION\\BART\\R\\python\\bulk-downloader"
+pyPath <- "D:\\RDEV2\\Kimberley_bfastSpatial\\bulk-downloader"
 pyScript <- "download_espa_order.py"
 py <- paste0(pyPath, "\\", pyScript)
 
 #Arguments
 email <- paste0("-e barthuntley@iinet.net.au")
-order <- paste0("-o ALL")
+order <- paste0("-o barthuntley@iinet.net.au-11032016-001812-872")
 target <- paste0("-d H:\\cdr_bulk")
 user <- "-u 30798574"
 pass <- "-p ba211069rt"
@@ -14,3 +14,32 @@ pass <- "-p ba211069rt"
 args <- paste(py, email, order, target, user, pass, sep =" ")
 
 shell(args)
+
+#barthuntley@iinet.net.au-11032016-001812-872
+barthuntley@iinet.net.au-11032016-001847-506
+barthuntley@iinet.net.au-11032016-001925-777
+
+##In a loop
+#next order ID's
+a <- "barthuntley@iinet.net.au-11032016-001847-506"
+b <- "barthuntley@iinet.net.au-11032016-001925-777"
+
+ids <- c(a, b)
+
+for(id in ids){
+  #Python script and path
+  pyPath <- "D:\\RDEV2\\Kimberley_bfastSpatial\\bulk-downloader"
+  pyScript <- "download_espa_order.py"
+  py <- paste0(pyPath, "\\", pyScript)
+  
+  #Arguments
+  email <- paste0("-e barthuntley@iinet.net.au")
+  order <- paste0("-o ", id)
+  target <- paste0("-d H:\\cdr_bulk")
+  user <- "-u 30798574"
+  pass <- "-p ba211069rt"
+  
+  args <- paste(py, email, order, target, user, pass, sep =" ")
+  
+  shell(args)
+}
